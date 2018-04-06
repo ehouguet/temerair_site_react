daf538a2f2c62107250b31320933e275308562a3
HOUGUET <elouan.houguet@capgemini.com>
2018-04-06 16:11:39 +0200

    layout page game with bootstrap 4 react

--- src/PageGame/PageGame.js
+++ src/PageGame/PageGame.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import Game from './Game/Game';
 import pageGameStyle from './PageGame.css';
-// import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 class PageGame extends Component {
   render() {
     return (
-      <div className={pageGameStyle.pageGame}>
-        <div className="header"></div>
-        <div className="body">
-          <div className="game">
+      <Container className={pageGameStyle.pageGame}>
+        <Row className="header">
+          <Col></Col>
+          <Col className="menu-right">
+            <Row className="list-joueur"></Row>
+            <Row className="chat"></Row>
+          </Col>
+        </Row>
+        <Row className="body">
+          <Col className="game">
             <Game is-multi={true}/>
-          </div>
-          <div className="menu-right">
-            <div className="list-joueur"></div>
-            <div className="chat"></div>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
